e7b9590cd35c33f5439699d46ca1b2d5d35b5c04
VladislavDyachenko2005 <125889654+VladislavDyachenko2005@users.noreply.github.com>
2023-03-20 16:02:57 +0300

    3.1 ООП текстовый файл
    Компоненты файла f – целые числа, причём положительных чисел столько же, 
    сколько отрицательных. Получить файл g из чисел исходного файла, в котором 
    записаны 2 положительных числа, затем 2 отрицательных и т. д.

--- Source.cpp
+++ Source.cpp
@@ -1,89 +1,132 @@
-/*Дана последовательность ненулевых целых чисел, за которой следует 0. Напечатать сначала все отрицательные числа этой последовательности, затем - все
-положительные(в любом порядке)*/
 #include <iostream>
-//#include <conio.h>
+#include <fstream>
+#include <stdlib.h>
 using namespace std;
- 
+void readFile(ifstream& rfile);
+void plusMinusFiles(ofstream &, ofstream&, ifstream&);
+void fileGWriting(ifstream&, ifstream&, ofstream&);
 
-void cikl(int* ar2, int K);
+
+int main() {
+
+	ofstream fFile("file.txt");
+	if (!fFile)
+	{
+		cout << "error1" << endl;
+		return 1;
+	}
+	int n;
+	int ch;
+	/*cout << "1 - static array, 2 - dynamic array; your number: ";
+	cin >> ch;*/
+	cout << "kol-vo chisel v fayle = ";
+	cin >> n;
+	/*enum eArrayInit
+	{
+		Uninited = 0, Static, Dynamic,
+	};
 
 
-void print(int* ar)
+	cout << " Выберите тип инициализации:\n"
+		<< Static << ".одномерный массив\n"
+		<< Dynamic << ".двумерный\n";
+	cout << "Любая другая кнопка завершит программу\n Ваш выбор: ";
+	switch(ch)
+	{ }*/
+	const int arrayfil[] = { 1, -2, 3, -3, -1, 2, 4, -4, -5, 5 };
+
+	int* arrFilDyn = new int [n];
+	/*cout << "input dynamic arrray. (amount of positive numbers = amount of negative numbers): ";
+	for (int i = 0; i < n; i++) {
+		cin >> arrFilDyn[i];
+	}*/
+	//функция создания файла	
+	for (int i = 0; i <= n - 1; i++)
+	{
+		int v = arrayfil[i];
+		fFile << v << endl;
+	}
+	/*for (int i = 0; i <= n - 1; i++)
+	{
+		int v = arrFilDyn[i];
+		fFile << v << endl;
+	}*/
+	fFile.flush();
+	ifstream rfile("file.txt");
+	if (!rfile)
+	{
+		cout << "error2" << endl;
+		return 2;
+	}
+	rfile.close();
+	rfile.clear();
+	rfile.open("file.txt");
+	readFile(rfile);
+	rfile.close();
+	rfile.clear();
+	rfile.open("file.txt");
+	ofstream minusFile("fileMinus.txt");
+	ofstream plusFile("filePlus.txt");
+	ifstream RminusFile("fileMinus.txt");
+	ifstream RplusFile("filePlus.txt");
+	plusMinusFiles(plusFile, minusFile, rfile);
+	cout << endl;
+	readFile(RminusFile);
+	cout << endl;
+	readFile(RplusFile);
+
+	ofstream gFile("fileG.txt");
+	RplusFile.close();
+	RplusFile.clear();
+	RplusFile.open("filePlus.txt");
+	RminusFile.close();
+	RminusFile.clear();
+	RminusFile.open("fileMinus.txt");
+	fileGWriting(RminusFile, RplusFile, gFile);
+	
+	ifstream grFile("fileG.txt");
+	readFile(grFile);
+
+	delete[]  arrFilDyn;
+	return 0;
+}
+void readFile(ifstream& rfile)
 {
-
-    if (*ar > 0)
-    {
-        print(ar + 1);
-        cout << *ar << " ";
-    }
-    else if (*ar < 0)
-    {
-        cout << *ar << " ";
-        print(ar + 1);
-    }
+	int v;
+	rfile >> v;
+	while (!rfile.fail())  //while( !infile.eof())
+	{
+		cout << v << " ";
+		rfile >> v;
+	}
+	cout << endl;
 }
- 
-int main()
-{
-    int N;
-    cout << "input N:" << endl;
-    cin >> N;
-    int K = N;
-
-    int* ar2 = new int[K];
-    int* ar = new int[N];
-    cout << "vvelite massiv 1 iz " << N << " chisel ne ravnih noly, 0 budet proignorirovan" << endl;
-    for (int i = 0; i < N - 1; i++) {
-        int a;
-        cin >> a;
-        if (a == 0) {
-            i--;
-            cout  << endl  << "chislo 0 propuscheno" << endl;
-            continue;
-        } else 
-            ar[i] = a;
-    }
-    //if (CheckValue != 1) {
-        ar[N - 1] = 0;
-        print(ar);
-        cout << ar[N - 1] << endl<< endl;
-    //}
-    
-    delete ar;
-    cout << "vvelite massiv 2 iz " << K << "chisel ne ravnih noly, 0 budet proignorirovan" << endl;
-    for (int i = 0; i < K; i++) {
-        int a;
-        cin >> a;
-        if (a == 0) {
-            i--;
-            cout << "chislo 0 propuscheno" << endl;
-            continue;
-        }
-        else
-            ar2[i] = a;
-    }
-   
-        ar2[K - 1] = 0;
-        cikl(ar2, N);
-  
-    return 0;
+void plusMinusFiles(ofstream& plusFile, ofstream& minusFile, ifstream& rfile) {
+	int v;
+	rfile >> v;
+	while (!rfile.eof()) {
+		//cout << v << " ";
+		if (v > 0)
+			plusFile << v << endl;
+		else
+			minusFile << v << endl;
+		rfile >> v;
+	}
+	
 }
-    /*for (int i = 0; i < N; i++) {
-        cout << endl;
-        cout << ar2[i] << " ";
-    }*/
-void cikl(int* ar2, int K) {
-    for (int i = 0; i < K; i++) {
-        if (ar2[i] < 0) {
-            cout << ar2[i] << " ";
-        }
-    }
-    for (int i = 0; i < K; i++) {
-        if (ar2[i] > 0) {
-            cout << ar2[i] << " ";
-        }
-    }
-    cout << ar2[K - 1];
+void fileGWriting(ifstream& RminusFile, ifstream& RplusFile, ofstream& gFile) {
+	int minus;
+	int plus;
+	RminusFile >> minus;
+	RplusFile >> plus;
+	while (!RminusFile.eof() && !RplusFile.eof()) {
+		for (int i = 0; i < 2; i++) {
+			gFile << plus << endl;
+			RplusFile >> plus;
+		}
+		for (int i = 0; i < 2; i++) {
+			gFile << minus << endl;
+			RminusFile >> minus;
+		}
+	}
 }
-
-
